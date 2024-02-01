545baee035b8dc9aa2e4d3721d8de67a51971b18
Syed Zeeshan Ahmed (Zee) <144811021+zeeawispo@users.noreply.github.com>
2024-02-01 11:32:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Content Replacement
-    const sidebarItems = document.querySelectorAll('aside ul li');
-    const mainSections = document.querySelectorAll('main section');
-
-    sidebarItems.forEach((item, index) => {
-        item.addEventListener('click', function () {
-            // Reset content in all sections
-            mainSections.forEach(section => section.style.display = 'none');
-            
-            // Display the corresponding section based on the clicked item
-            mainSections[index].style.display = 'block';
-        });
+    // DOM Manipulation using JavaScript
+    document.getElementById('sidebarList').addEventListener('click', function (event) {
+        var section = document.querySelector('section');
+        section.innerHTML = '<h2>Section 1</h2><p>Content changed by clicking on ' + event.target.textContent + '.</p>';
     });
 
-    // Style Alteration
-    const changeColorButton = document.getElementById('changeColorButton');
+    function changeColorScheme() {
+        document.body.style.backgroundColor = '#e74c3c';
+        document.body.style.color = '#fff';
+    }
 
-    changeColorButton.addEventListener('click', function () {
-        // Change background color and text color
-        document.body.style.backgroundColor = '#f0f0f0';
-        document.body.style.color = '#333';
-    })
+    function addItem() {
+        var newItem = prompt('Enter the new item:');
+        if (newItem) {
+            var sidebarList = document.getElementById('sidebarList');
+            var li = document.createElement('li');
+            li.textContent = newItem;
+            sidebarList.appendChild(li);
+        }
+    }
+
+    function removeItem() {
+        var sidebarList = document.getElementById('sidebarList');
+        if (sidebarList.children.length > 0) {
+            sidebarList.removeChild(sidebarList.lastElementChild);
+        }
+    }
+let alertshown=false;
+    document.querySelector('header').addEventListener('mouseover', function () {
+        if(!alertshown)
+        {
+            alert('Welcome to My Portfolio');
+            alertshown=true;
+        }
+    });
+
+    function changeImage(imageid)
+    {
+        console.log("CHANGE IMAGE TO: " + imageid);
+        document.getElementById("myimage").src = "./assests/Image_" + imageid + ".png";
+        document.getElementById("imagelabel").innerHTML=imagelabels[imageid-1]
+    }
+    const imagelabels = ["services","California","Niagra"];
+    function toggleFunction() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+     }
